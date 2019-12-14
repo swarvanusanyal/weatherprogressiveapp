6ec29d92fc078514f1921110497e191c22e810aa
swsanyal <swarvanu.sanyal@here.com>
2019-12-14 16:56:41 -0600

    dynamic port

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ function startServer() {
   app.use(express.static('public'));
 
   // Start the server
-  return app.listen('8000', () => {
+  return app.listen(process.env.PORT || '8000', () => {
     // eslint-disable-next-line no-console
     console.log('Local DevServer Started on port 8000...');
   });
