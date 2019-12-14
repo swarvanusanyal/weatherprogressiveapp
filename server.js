9d243c95af52c417391df693a00d612e7b360b19
swsanyal <swarvanu.sanyal@here.com>
2019-12-14 16:51:21 -0600

    change port

--- server.js
+++ server.js
@@ -189,9 +189,9 @@ function startServer() {
   app.use(express.static('public'));
 
   // Start the server
-  return app.listen('8001', () => {
+  return app.listen('8000', () => {
     // eslint-disable-next-line no-console
-    console.log('Local DevServer Started on port 8001...');
+    console.log('Local DevServer Started on port 8000...');
   });
 }
 
